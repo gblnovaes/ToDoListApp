23698f34a20a9a464cc1f0545dc991e9afe038a4
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2021-06-27 00:38:55 -0300

    implemented pass data to fill fields in update fragment

--- app/src/main/java/br/com/gabrielnovaes/todoapp/update/UpdateFragment.kt
+++ app/src/main/java/br/com/gabrielnovaes/todoapp/update/UpdateFragment.kt
@@ -3,21 +3,39 @@ package br.com.gabrielnovaes.todoapp.update
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import br.com.gabrielnovaes.todoapp.R
+import br.com.gabrielnovaes.todoapp.data.models.Priority
+import kotlinx.android.synthetic.main.fragment_update.view.*
+import kotlinx.android.synthetic.main.row_layout.view.*
 
 class UpdateFragment : Fragment() {
-
+    private val args by navArgs<UpdateFragmentArgs>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        val view  = inflater.inflate(R.layout.fragment_update, container, false)
+
+        setHasOptionsMenu(true)
+
+        view.current_title_et.setText(args.currentItem.title)
+        view.current_description_et.setText(args.currentItem.description)
+        view.current_priorities_spinner.setSelection(parsePriority(args.currentItem.priority))
+
+        return view
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.update_fragment_menu,menu)
     }
 
+    private fun parsePriority(priority: Priority) : Int{
+        return when(priority){
+            Priority.HIGH -> 0
+            Priority.MEDIUM -> 1
+            Priority.LOW -> 2
+        }
+    }
 }
