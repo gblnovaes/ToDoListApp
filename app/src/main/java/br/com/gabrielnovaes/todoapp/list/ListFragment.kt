93bfa9f56d0a12eab85fdbd6c5b68f2f15eba6fd
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2021-07-03 16:33:31 -0300

    add binding adapter to show error image in not data

--- app/src/main/java/br/com/gabrielnovaes/todoapp/list/ListFragment.kt
+++ app/src/main/java/br/com/gabrielnovaes/todoapp/list/ListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.*
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -12,63 +13,51 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.gabrielnovaes.todoapp.R
 import br.com.gabrielnovaes.todoapp.SharedViewModel
 import br.com.gabrielnovaes.todoapp.data.viewmodel.ToDoViewModel
+import br.com.gabrielnovaes.todoapp.databinding.FragmentListBinding
 import kotlinx.android.synthetic.main.fragment_list.*
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
 
-    private val adapter : ListAdapter by lazy { ListAdapter() }
-    private val mToDoViewModel : ToDoViewModel by viewModels()
-    private val mSharedViewModel : SharedViewModel by viewModels()
+    private val adapter: ListAdapter by lazy { ListAdapter() }
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_list, container, false)
 
-        val recyclerView = view.recyclerView
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mSharedViewModel = mSharedViewModel
 
+        setupRecyclerView()
         mToDoViewModel.getAllData.observe(viewLifecycleOwner, { data ->
             mSharedViewModel.checkEmptyDatabaseEmpty(data)
             adapter.setData(data)
         })
 
-        view.floatingActionButton.setOnClickListener {
-            findNavController().navigate(R.id.action_listFragment_to_addFragment)
-        }
-
-        mSharedViewModel.emptyDatabaseEmpty.observe(viewLifecycleOwner, {
-            showEmptyDatabaseViews(it)
-        })
-
         setHasOptionsMenu(true)
-        return view
+
+        return binding.root
     }
 
-    private fun showEmptyDatabaseViews(emptyListItem : Boolean) {
-        when {
-            emptyListItem -> {
-                boxEmpty.visibility = View.VISIBLE
-                lblBoxEmpty.visibility = View.VISIBLE
-            }
-            else -> {
-                boxEmpty.visibility = View.INVISIBLE
-                lblBoxEmpty.visibility = View.INVISIBLE
-            }
-        }
-    }
+    private fun setupRecyclerView() {
+        val recyclerView = binding.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
 
+    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.list_fragment_menu,menu)
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.menu_delete_all -> confirmDeleteAllItem()
         }
         return super.onOptionsItemSelected(item)
@@ -77,16 +66,20 @@ class ListFragment : Fragment() {
     private fun confirmDeleteAllItem() {
         val builder = AlertDialog.Builder(requireContext())
 
-        builder.setPositiveButton("Yes"){_,_ ->
+        builder.setPositiveButton("Yes") { _, _ ->
             mToDoViewModel.deleteAllData()
             Toast.makeText(context, "Successfully Removed Everything!!", Toast.LENGTH_SHORT).show()
         }
 
-        builder.setNegativeButton("No"){_,_ -> }
+        builder.setNegativeButton("No") { _, _ -> }
         builder.setTitle("Delete everything?")
         builder.setMessage("Are you sure want to remove everything?")
         builder.create().show()
 
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
